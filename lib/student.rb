29497c800b30af0e0ce81a5aaff684cd2fc1bee9
Maria Nunez <mariaalician@gmail.com>
2018-07-10 23:44:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+def initialize
+  @knowledge = Array.new
+end
+
+def learn
+  @knowledge << knowledge
+
+end
+
+def knowledge
+  @knowledge
+end
 
 end
